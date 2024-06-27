8a355691fd90f1b9c6a880a6f258a8bc73e35bef
alexeyroden <galadmin@rambler.ru>
2024-06-27 15:21:50 +0300

    Add files via upload

---
+++ kayboard.go
@@ -0,0 +1,20 @@
+package kayboard
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+func GetFloat() (float64,error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return -1,err
+	}
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input,64)
+	if err != nil {
+	return -1, err
+	}
+	return number, nil
+}
